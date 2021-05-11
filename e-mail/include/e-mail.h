a78d1db0e6b7b362764383f79ba4c9e34e822fdb
Feedbaek <nicek789@naver.com>
2021-05-11 16:05:43 +0900

    2

---
+++ e-mail/include/e-mail.h
@@ -0,0 +1,14 @@
+#ifndef E-MAIL_H
+# define E-MAIL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct a
+{
+    struct s_person *friends[5];
+    int *relation;
+}   A;
+
+
+#endif
---
+++ e-mail/srcs/e-mail.c
@@ -0,0 +1,7 @@
+#include "e-mail.h"
+
+int     main(void) 
+{
+    int friends[11][11] = 
+    return (0);
+}
