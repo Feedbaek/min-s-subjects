8075cbb907d954dbe1ddcd1383eb1e5ebd274723
feedbaek <nicek789@naver.com>
2021-10-11 20:20:03 +0900

    21.10.11

--- extended_Euclid/main.c
+++ extended_Euclid/main.c
@@ -12,7 +12,7 @@ struct _node	*extended_Euclid(int a, int b)	// 확장 유클리드 함수
 {
 	struct _node	*n1, *n2;	// 구조체를 가르키는 포인터 변수
 
-	n1 = (struct _node *)malloc(sizeof(struct _node));	// 구조체 만들어서 n1에 담음.
+	n1 = (struct _node *)malloc(sizeof(struct _node));	// 구조체 만들어서 n1에 할당.
 	if (b == 0)		// 계속 재귀하다가 b가 0이 될 때 (재귀함수 탈출 조건)
 	{
 		n1->first = 1;
@@ -52,7 +52,7 @@ int	main(void)
 		}
 		n = extended_Euclid(a, b);	// 재귀 호출해서 나온 값을 n에 담음
 		printf("x is %d, y is %d, and c is %d\n", n->first, n->second, n->third);
-		free(n);		// 사용끝난 구조체 해제
+		free(n);		// 사용끝난 구조체 할당해제
 		break ;
 	}
 	return (0);
